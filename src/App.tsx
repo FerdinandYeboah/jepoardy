3ffe3ec45f8ab8e0040688ee56739d47f64d87f6
FerdinandYeboah <FerdinandYeboah@gmail.com>
2020-03-12 22:54:13 -0400

    Added react-router, antDesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+import Home from './screens/Home'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    return (
+      // Use browser router instead of router for website (as opposed to mobile app) routing.
+      <BrowserRouter>
+
+        {/* ADD ALL PATHS FOR THE APP HERE. INCLUDING NESTED PATHS i.e /register/student */}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/lobby" component={Home} />
+        <Route exact path="/game-board" component={Home} />
+        <Route exact path="/question" component={Home} />
+
+        {/* Use Link to get to the route whenever needed from anywhere */}
+
+    </BrowserRouter>
+    )
 }
-
-export default App;
