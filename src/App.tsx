3c1404c32a33ee90951b4f5e6d47a2ece493c642
FerdinandYeboah <FerdinandYeboah@gmail.com>
2020-03-25 19:43:35 -0400

    Add socketio client and attempt first global context

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,31 @@ import UpcomingQuestion from './components/game/UpcomingQuestion'
 import { defaultUpcoming } from './models/UpcomingQuestionModel'
 import GameQuestion from './components/game/GameQuestion'
 import { defaultGameQuestion } from './models/GameQuestionModel'
+import { GlobalContextProvider } from './context/globalContext'
 
 export default function App(){
     return (
-      // Use browser router instead of router for website (as opposed to mobile app) routing.
-      <BrowserRouter>
 
-        {/* ADD ALL PATHS FOR THE APP HERE. INCLUDING NESTED PATHS i.e /register/student */}
-        <Route exact path="/" component={Home} />
-        <Route exact path="/lobby" component={Lobby} />
-        <Route exact path="/create-room" component={CreateRoom} />
-        <Route exact path="/waiting-room" component={WaitingRoom} />
-        <Route exact path="/game-board" component={GameBoard} />
+      // <GlobalContextProvider>
+        // {/* // Use browser router instead of router for website (as opposed to mobile app) routing. */}
+        <BrowserRouter>
+
+          {/* ADD ALL PATHS FOR THE APP HERE. INCLUDING NESTED PATHS i.e /register/student */}
+          <Route exact path="/" component={Home} />
+          <Route exact path="/lobby" component={Lobby} />
+          <Route exact path="/create-room" component={CreateRoom} />
+          <Route exact path="/waiting-room" component={WaitingRoom} />
+          <Route exact path="/game-board" component={GameBoard} />
 
 
-        {/* TESTING COMPONENTS that later may not have own routes */}
-        {/* <Route exact path="/upcoming-question"><UpcomingQuestion category="Default" value={200}/></Route> */}
-        <Route exact path="/upcoming-question"><UpcomingQuestion {...defaultUpcoming}/></Route>
-        <Route exact path="/game-question"><GameQuestion {...defaultGameQuestion}/></Route>
+          {/* TESTING COMPONENTS that later may not have own routes */}
+          {/* <Route exact path="/upcoming-question"><UpcomingQuestion category="Default" value={200}/></Route> */}
+          <Route exact path="/upcoming-question"><UpcomingQuestion {...defaultUpcoming}/></Route>
+          <Route exact path="/game-question"><GameQuestion {...defaultGameQuestion}/></Route>
 
-        {/* Use Link to get to the route whenever needed from anywhere */}
+          {/* Use Link to get to the route whenever needed from anywhere */}
 
-    </BrowserRouter>
+        </BrowserRouter>
+      // </GlobalContextProvider>
     )
 }
---
+++ src/context/globalContext.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, useContext } from "react";
+import io from "socket.io-client";
+
+// Create context and create hook for it
+const GlobalContext = React.createContext()
+
+// Create hook for context. Will be consumed
+export function useGlobalContext() {
+    return useContext(GlobalContext)
+}
+
+// Create Provider (React Component) for context. Will be consumed
+export function GlobalContextProvider(children) {
+    const [socket, setSocket] = useState();
+
+    //Initialization logic
+    useEffect(() => {
+        console.log("Called useEffect");
+        //Connect socket
+        const socket = io("http://localhost:3001");
+        setSocket(socket);
+        console.log("Client socket id: ", socket.id);
+
+        //Add listeners? Nah I think add listeners later in relevant child components.
+
+    }, [])
+
+    //Return back provider exposing global data and methods. Could create a socket.on/emit proxy method
+    return (
+        <GlobalContextProvider value={
+            socket
+        }>
+            {children}
+        </GlobalContextProvider>
+    )
+}
