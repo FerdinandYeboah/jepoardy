ab0fe551c51635ab418324fa2b92409deba591b3
FerdinandYeboah <FerdinandYeboah@gmail.com>
2020-07-01 00:10:12 -0400

    Add build folder since cloud vm too small to build

---
+++ build/precache-manifest.fb195855f69b0b9aa2dfd8fe6ef58e77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e9046f9bc09c577fe7e4c9244d8f74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5003cfdc53f880a36db3",
+    "url": "/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "c770176830670fde8bb4",
+    "url": "/static/css/main.8a89c3c4.chunk.css"
+  },
+  {
+    "revision": "5003cfdc53f880a36db3",
+    "url": "/static/js/2.98a614dd.chunk.js"
+  },
+  {
+    "revision": "b87b470800d58f66c7cc31828b6c2776",
+    "url": "/static/js/2.98a614dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c770176830670fde8bb4",
+    "url": "/static/js/main.291f7636.chunk.js"
+  },
+  {
+    "revision": "f327c0869edbb167a056",
+    "url": "/static/js/runtime-main.08a4b1d9.js"
+  }
+]);
--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #Run as ./run.sh
 
-#Build static files and copy to root?
-npm run build
-mkdir -p ~/sites/jeopardy/build
-cp -r build ~/sites/jeopardy/build
-
 #Serve with nginx
 export NGINX_JEOPARDY_FOLDER=$PWD/nginx
 export NGINX_LOG_PATH=$PWD/nginx
